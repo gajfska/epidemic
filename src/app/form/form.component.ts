63786ce60428df9475b3d39e32a2e27a13554861
Werka <weronika.gajska@gmail.com>
2021-01-13 22:57:44 +0100

    Adding style to app component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,8 @@
 import {Component, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {EpidemicModel} from '../shared/epidemic.model';
+import {EpidemicService} from '../shared/epidemic.service';
+
 
 @Component({
     selector: 'app-form',
@@ -9,14 +12,17 @@ import {NgForm} from '@angular/forms';
 export class FormComponent {
     @ViewChild('f', {static: false}) slForm: NgForm;
 
+    constructor(private epidemicService: EpidemicService) {
+    }
 
-    onSubmit(form: NgForm): void {
+
+        onSubmit(form: NgForm): void {
         if (!form.valid) {
             return;
         }
         const value = form.value;
-
-        console.log(form.value);
-        console.log(value.populationSize);
+        // const newSimulation = new EpidemicModel(value);
+        this.epidemicService.addModel(value);
+        form.resetForm();
     }
 }
