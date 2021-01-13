294c5b1059a8d5c96d43e1af4e2018c3068c39e3
Werka <weronika.gajska@gmail.com>
2021-01-13 23:44:21 +0100

    changing interface to class, adding uuid

--- src/app/shared/epidemic.model.ts
+++ src/app/shared/epidemic.model.ts
@@ -1,4 +1,7 @@
-export interface EpidemicModel {
+import { v4 as uuidv4 } from 'uuid';
+
+export class EpidemicModel {
+    id = uuidv4();
     nameSimulation: string;
     populationSize: number;
     initianNumberOfInfectedPeople: number;
@@ -7,4 +10,17 @@ export interface EpidemicModel {
     numberDayInfectionToRecover: number;
     numberDayInfectionToDeath: number;
     numberDaySimulationPerformed: number;
+
+    constructor(nameSimulation: string, populationSize: number, initianNumberOfInfectedPeople: number, indexOfR: number,
+                mortalityRate: number, numberDayInfectionToRecover: number, numberDayInfectionToDeath: number,
+                numberDaySimulationPerformed: number) {
+        this.nameSimulation = nameSimulation;
+        this.populationSize = populationSize;
+        this.initianNumberOfInfectedPeople = initianNumberOfInfectedPeople;
+        this.indexOfR = indexOfR;
+        this.mortalityRate = mortalityRate;
+        this.numberDayInfectionToRecover = numberDayInfectionToRecover;
+        this.numberDayInfectionToDeath = numberDayInfectionToDeath;
+        this.numberDaySimulationPerformed = numberDaySimulationPerformed;
+    }
 }
