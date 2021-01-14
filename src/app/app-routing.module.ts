adb6c078a6396abde5000cf567f34e7784d74f54
Werka <weronika.gajska@gmail.com>
2021-01-14 19:20:45 +0100

    Adding routing to project (main-page)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
+import {MainPageComponent} from './main-page/main-page.component';
+import {FormComponent} from './main-page/form/form.component';
+import {TableComponent} from './main-page/table/table.component';
 
 
 const appRoutes: Routes = [
-    {path: '', redirectTo: '/register', pathMatch: 'full'},
+    {path: '', redirectTo: '/main-page', pathMatch: 'full'},
+    {path: 'main-page', component: MainPageComponent},
+
 ];
 
 
